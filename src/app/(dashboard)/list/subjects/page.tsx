d906c274fa8fa082686a837496a80ca0cb1ba712
John Laurence Resando <83331866+Looreenzo0@users.noreply.github.com>
2024-09-27 22:55:18 +0800

    Big calendar

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -2,9 +2,9 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { role, subjectsData } from "@/lib/data";
 import prisma from "@/lib/prisma";
 import { ITEM_PER_PAGE } from "@/lib/settings";
+import { role } from "@/lib/utils";
 import { Prisma, Subject, Teacher } from "@prisma/client";
 import Image from "next/image";
 
@@ -20,10 +20,14 @@ const columns = [
     accessor: "teachers",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Actions",
-    accessor: "action",
-  },
+  ...(role === "admin"
+    ? [
+        {
+          header: "Actions",
+          accessor: "action",
+        },
+      ]
+    : []),
 ];
 
 const renderRow = (item: SubjectList) => (
